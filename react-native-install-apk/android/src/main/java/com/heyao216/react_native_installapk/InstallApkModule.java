8f27464393e8a21a723bbb7e32cfd2d248fa36a8
1461413652 <1461413652@qq.com>
2019-11-26 11:07:39 +0800

    Update InstallApkModule.java
    增加android7.0下载apk判断

--- react-native-install-apk/android/src/main/java/com/heyao216/react_native_installapk/InstallApkModule.java
+++ react-native-install-apk/android/src/main/java/com/heyao216/react_native_installapk/InstallApkModule.java
@@ -2,6 +2,8 @@ package com.heyao216.react_native_installapk;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.content.FileProvider;
+import android.os.Build;
 
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -36,8 +38,21 @@ public class InstallApkModule extends ReactContextBaseJavaModule {
             e.printStackTrace();
         }
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setDataAndType(Uri.parse("file://" + path),"application/vnd.android.package-archive");
+        Uri data;
+        File file =new File(path);
+        if(!file.exists()) {
+            return;
+        }
+        // 判断版本大于等于7.0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            // "com.heyao216.fileprovider"即是在清单文件中配置的authorities
+            data = FileProvider.getUriForFile(_context, "com.rn_pda.fileprovider", file);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        } else {
+            data = Uri.fromFile(file);
+            // intent.setDataAndType(Uri.parse("file://" + path),"application/vnd.android.package-archive");
+        }
+        intent.setDataAndType(data, "application/vnd.android.package-archive");
         _context.startActivity(intent);
     }
 }
